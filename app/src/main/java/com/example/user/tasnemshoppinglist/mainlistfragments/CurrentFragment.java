aba086fb634d34f120a304da9374525a9f158fb5
user <tasnem.mobarky@gmail.com>
2017-10-25 13:23:03 +0300

    tt

--- app/src/main/java/com/example/user/tasnemshoppinglist/mainlistfragments/CurrentFragment.java
+++ app/src/main/java/com/example/user/tasnemshoppinglist/mainlistfragments/CurrentFragment.java
@@ -6,13 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.user.tasnemshoppinglist.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CurrentFragment extends Fragment {
+    public class CurrentFragment extends Fragment
+{
+    private TextView tvTotal,tvTotalvalue,tvCount,tvCountValue;
+    private ImageButton imbSave;
+    private ListView lstvCurrent;
+
 
 
     public CurrentFragment() {
@@ -24,7 +34,18 @@ public class CurrentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_current, container, false);
+        tvTotal=(TextView) view.findViewById(R.id.tvTotal);
+        tvTotalvalue=(TextView) view.findViewById(R.id.tvTotalvalue);
+        tvCount=(TextView)view.findViewById(R.id.tvCount);
+        tvCountValue=(TextView) view.findViewById(R.id.tvCountValue);
+        imbSave=(ImageButton) view.findViewById(R.id.imbSave);
+        lstvCurrent=(ListView)view.findViewById(R.id.lstvCurrent);
+        String[]ar={"tasnem","haya","adham","sozan"};
+        ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(this,)
+     return view;
     }
+
 
 }
--- app/src/main/java/com/example/user/tasnemshoppinglist/mainlistfragments/HistoryFragment.java
+++ app/src/main/java/com/example/user/tasnemshoppinglist/mainlistfragments/HistoryFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.user.tasnemshoppinglist.R;
 
@@ -13,6 +14,7 @@ import com.example.user.tasnemshoppinglist.R;
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
+    private ListView lstvHistory;
 
 
     public HistoryFragment() {
@@ -24,7 +26,9 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view= inflater.inflate(R.layout.fragment_history, container, false);
+        lstvHistory=(ListView) view.findViewById(R.id.lstvHistory);
+        return view;
     }
 
 }
